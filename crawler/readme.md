65a4647aff531cb0427027ca96e105c3c06878bf
kuomay <40718005@stu.nkuht.edu.tw>
2022-12-17 12:03:14 +0800

    add .env

---
+++ crawler/readme.md
@@ -0,0 +1,7 @@
+# Crawler
+
+# Installation
+
+# cp .env.example .env
+
+# config your database username and password
--- crawler/test.js
+++ crawler/test.js
@@ -2,17 +2,23 @@
 
 const mysql2 = require('mysql2/promise');
 
+// npm i dotenv
+require('dotenv').config();
+// let dotenv = require('dotenv');
+// dotenv.config();
+
 (async () => {
-  const connection = await mysql2.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'admin',
-    password: '',
-    database: 'stock_mfee31',
+    console.log(process.env.DB_USER);
+    const connection = await mysql2.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
   });
 
   // simple query
-  let result = await connection.query('SELECT * FROM `stocks`');
+  let [data, fields] = await connection.query('SELECT * FROM `stocks`');
   //let data = result[0];
   // console.log(result);
   console.log(data);
